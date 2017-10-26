16c999dc3e489141b03815793d6e9e32f1768fef
Shane Cavanaugh <shane@shanecav.net>
2017-10-25 23:59:48 -0400

    Install rspec

--- test/dummy/app/jobs/application_job.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationJob < ActiveJob::Base
-  work_as_pendant
-
-  def perform
-    p 'Perform Application Job'
-  end
-end
--- test/integration/jobs/application_job_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ApplicationJobTest < ActiveJob::TestCase
-  test 'work_as_pending' do
-    ApplicationJob.perform_now
-  end
-end
