cbd2bc2404a4ba5c0f7ecb311078fb6466a29fc5
Shane Cavanaugh <shane@shanecav.net>
2017-10-25 23:14:47 -0400

    Update channel name

--- lib/pendant/work_as_pendant.rb
+++ lib/pendant/work_as_pendant.rb
@@ -12,11 +12,7 @@ module Pendant
 
     module InstanceMethods
       def broadcase_success
-        ActionCable.server.broadcast pendant_channel, status: 'success'
-      end
-
-      def pendant_channel
-        "pendant_channel_#{job_id}"
+        ActionCable.server.broadcast 'pendant_channel', status: 'success'
       end
     end
   end
