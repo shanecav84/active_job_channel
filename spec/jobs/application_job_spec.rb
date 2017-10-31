ddd503949c927e728922419022ae59c00d558bad
Shane Cavanaugh <shane@shanecav.net>
2017-10-30 23:56:50 -0400

    Test for job_name

--- spec/jobs/application_job_spec.rb
+++ spec/jobs/application_job_spec.rb
@@ -1,40 +1,27 @@
 require 'spec_helper'
 
 RSpec.describe ApplicationJob do
-  before { described_class.pendants = [] }
-
   it 'broadcasts success' do
-    job_id = 123
-    allow_any_instance_of(described_class).
-      to receive(:job_id).
-      and_return(job_id)
     expect(::PendantChannel).
       to receive(:broadcast_to).
-      with('pendant_channel', status: 'success', job_id: job_id)
+      with('pendant_channel', status: 'success', job_name: 'Job Name')
     described_class.perform_now
   end
 
   it 'broadcasts failure' do
-    job_id = 123
     allow_any_instance_of(described_class).
       to receive(:fake).
       and_raise(::StandardError)
-    allow_any_instance_of(described_class).
-      to receive(:job_id).
-      and_return(job_id)
     expect(::PendantChannel).
       to receive(:broadcast_to).
-      with('pendant_channel', status: 'failure', job_id: job_id)
+      with('pendant_channel', status: 'failure', job_name: 'Job Name')
     expect { described_class.perform_now }.to raise_error(::StandardError)
   end
 
-  it 'increments list of pendants' do
-    job_id = 123
-    allow_any_instance_of(described_class).
-      to receive(:job_id).
-      and_return(job_id)
-    expect(described_class.pendants).to eq([])
-    expect { described_class.perform_now }.
-      to change { described_class.pendants }.to([job_id])
+  it 'broadcasts the job name' do
+    expect(::PendantChannel).
+      to receive(:broadcast_to).
+      with('pendant_channel', status: 'success', job_name: 'Job Name')
+    described_class.perform_now
   end
 end
