c47dda95e1edda54f0d5875878775d990421f6cb
Shane Cavanaugh <shane@shanecav.net>
2017-10-31 15:57:39 -0400

    Don't depend on CSS classes for now

--- app/assets/javascripts/pendant/channels/pendant.js
+++ app/assets/javascripts/pendant/channels/pendant.js
@@ -1,31 +1,27 @@
 document.addEventListener("DOMContentLoaded", function (_event) {
   const CHANNEL = "::PendantChannel";
-  const pendant_classes = document.getElementsByClassName('pendant');
+  console.log('Subscribing to ' + CHANNEL);
 
-  for (var i = 0; i < pendant_classes.length; i++) {
-    console.log('Subscribing to ' + CHANNEL);
-
-    App.pendant = App.pendant_cable.subscriptions.create(
-      {
-        channel: CHANNEL
+  App.pendant = App.pendant_cable.subscriptions.create(
+    {
+      channel: CHANNEL
+    },
+    {
+      connected: function () {
+        // Called when the subscription is ready for use on the server
+        console.log('Successfully connected to ' + CHANNEL);
       },
-      {
-        connected: function () {
-          // Called when the subscription is ready for use on the server
-          console.log('Successfully connected to ' + CHANNEL);
-        },
 
-        disconnected: function () {
-          // Called when the subscription has been terminated by the server
-          console.log('Successfully disconnected from ' + CHANNEL);
-        },
+      disconnected: function () {
+        // Called when the subscription has been terminated by the server
+        console.log('Successfully disconnected from ' + CHANNEL);
+      },
 
-        received: function (data) {
-          // Called when there's incoming data on the websocket for this channel
-          console.log('Successfully received data from ' + CHANNEL);
-          console.log(data);
-        }
+      received: function (data) {
+        // Called when there's incoming data on the websocket for this channel
+        console.log('Successfully received data from ' + CHANNEL);
+        console.log(data);
       }
-    );
-  }
+    }
+  );
 });
