9e94dc39a6f5498ff7eb991d31a0335cf16388bb
Shane Cavanaugh <shane@shanecav.net>
2017-12-01 21:59:06 -0500

    Remove job_name attr

--- lib/active_job_notifier/notifier.rb
+++ lib/active_job_notifier/notifier.rb
@@ -2,8 +2,6 @@ module ActiveJobNotifier
   module Notifier
     module ClassMethods
       def active_job_notifier(options = {})
-        mattr_accessor(:job_name) { options[:job_name] }
-
         after_perform :broadcast_success
         rescue_from '::StandardError' do |exception|
           broadcast_failure
@@ -19,7 +17,7 @@ module ActiveJobNotifier
         ActiveJobNotifierChannel.broadcast_to(
           'active_job_notifier_channel',
           status: 'failure',
-          job_name: self.class.job_name
+          job_name: self.class.to_s
         )
       end
 
@@ -27,7 +25,7 @@ module ActiveJobNotifier
         ActiveJobNotifierChannel.broadcast_to(
           'active_job_notifier_channel',
           status: 'success',
-          job_name: self.class.job_name
+          job_name: self.class.to_s
         )
       end
     end
