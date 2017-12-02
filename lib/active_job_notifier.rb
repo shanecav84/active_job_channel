4e5df48568ae364e1639674d38ec62203e2380bc
Shane Cavanaugh <shane@shanecav.net>
2017-12-02 11:11:15 -0500

    Require ActionCable and ActiveJob before loading setting up ActiveJobNotifier

--- lib/active_job_notifier.rb
+++ lib/active_job_notifier.rb
@@ -1,5 +1,7 @@
 require 'active_job_notifier/engine'
 require 'active_job_notifier/notifier'
+require 'action_cable/engine'
+require 'active_job/railtie'
 
 # Top-level namespace
 module ActiveJobNotifier
