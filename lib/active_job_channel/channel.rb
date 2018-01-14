52add2f0273ef965d83fdfaba58adbb8f2188887
Shane Cavanaugh <shane@shanecav.net>
2018-01-13 20:00:56 -0500

    Use connection identifier to stream from, if available

--- lib/active_job_channel/channel.rb
+++ lib/active_job_channel/channel.rb
@@ -1,7 +1,15 @@
 module ActiveJobChannel
   class Channel < ::ActionCable::Channel::Base
+    CHANNEL_NAME = 'active_job_channel'.freeze
+
     def subscribed
-      stream_from 'active_job_channel'
+      stream_from channel_name
+    end
+
+    private
+
+    def channel_name
+      [CHANNEL_NAME, connection.connection_identifier].compact.join('#')
     end
   end
 end
