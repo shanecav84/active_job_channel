d5a48d023b759aba8418d307aa28ea04bd9f8ea4
Shane Cavanaugh <shane@shanecav.net>
2018-01-13 20:55:29 -0500

    Stream from both the global and private streams

--- lib/active_job_channel/channel.rb
+++ lib/active_job_channel/channel.rb
@@ -3,13 +3,18 @@ module ActiveJobChannel
     CHANNEL_NAME = 'active_job_channel'.freeze
 
     def subscribed
-      stream_from channel_name
+      stream_from CHANNEL_NAME
+      stream_from private_stream if connection.connection_identifier.present?
     end
 
     private
 
-    def channel_name
-      [CHANNEL_NAME, connection.connection_identifier].compact.join('#')
+    def global_stream
+      stream_from CHANNEL_NAME
+    end
+
+    def private_stream
+      [CHANNEL_NAME, connection.connection_identifier].join('#')
     end
   end
 end
