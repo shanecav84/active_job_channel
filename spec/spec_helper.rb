c5599526c423cf020865ff759a791854b7e4a930
Shane Cavanaugh <shane@shanecav.net>
2018-01-14 22:46:39 -0500

    Remove unnecessary config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
 require 'rspec'
 
-ActiveJob::Base.queue_adapter = :test
-
 RSpec.configure do |config|
   config.mock_with :rspec
   config.order = :random
