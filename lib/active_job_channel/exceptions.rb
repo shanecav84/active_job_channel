c7c40ccf3739d3a3c0625fd309d055767a31f89b
Shane Cavanaugh <shane@shanecav.net>
2018-04-28 11:23:11 -0400

    Shorter error messages

--- lib/active_job_channel/exceptions.rb
+++ lib/active_job_channel/exceptions.rb
@@ -13,15 +13,7 @@ module ActiveJobChannel
 
     def generate_message
       'ActiveJobChannel has been configured to broadcast privately for ' \
-        "#{job_class}, but an `ajc_identifier` has not been set. The " \
-        'identifier should be made available in your job via a method or an ' \
-        'instance variable, either named `ajc_identifier`. For details about ' \
-        'setting up an identifier in your ActionCable Connection, visit ' \
-        'http://guides.rubyonrails.org/' \
-        "action_cable_overview.html#connection-setup\n\nTo broadcast " \
-        'publically without an identifier, pass in ' \
-        '`{ public_broadcast: true }` to `active_job_channel` as part of an ' \
-        'options hash.'
+        "#{job_class}, but an `ajc_identifier` has not been set."
     end
   end
 
@@ -37,10 +29,7 @@ module ActiveJobChannel
 
     def generate_message
       'ActiveJobChannel has been configured to broadcast globally for ' \
-        "#{job_class}, but an `ajc_identifier` has been set. If the job " \
-        'information should be broadcast publically, remove the ' \
-        '`ajc_identifier`. Otherwise, pass in `{ public_broadcast: true } ` ' \
-        'to `active_job_channel` as part of an options hash'
+        "#{job_class}, but an `ajc_identifier` has been set."
     end
   end
 end
